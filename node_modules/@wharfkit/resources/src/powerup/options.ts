10409e3288b3590d6af56e914edfe32e6423225e
maestrobeatz <maestrobeatz805@gmail.com>
2024-08-15 23:54:02 -0700

    Add .gitignore to exclude node_modules and other unnecessary files

--- node_modules/@wharfkit/resources/src/powerup/options.ts
+++
@@ -1,9 +0,0 @@
-import {TimePointType, UInt64} from '@wharfkit/antelope'
-
-export interface PowerUpStateOptions {
-    // timestamp to base adjusted_utilization off
-    timestamp?: TimePointType
-    // blockchain resource limits for calculating usage
-    virtual_block_cpu_limit?: UInt64
-    virtual_block_net_limit?: UInt64
-}
